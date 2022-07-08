cf6ab182abccac7df1073d1d858e261bc01107eb
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-07-09 00:58:52 +0530

    added:addressbook

---
+++ addressbook.java
@@ -0,0 +1,42 @@
+package com.bridge.labzs;
+
+import java.util.Scanner;
+public class addressbook {
+    public static void main(String args[]) {
+
+            Scanner scan = new Scanner(System.in);
+        Contacts contacts = new Contacts();
+        System.out.println("FirstName: ");
+        String firstName = scan.next();
+        contacts.setFirstName(firstName);
+        System.out.println("LastName: ");
+        String lastName = scan.next();
+        contacts.setLastName(lastName);
+        System.out.println("Address: ");
+        String address = scan.next();
+        contacts.setAddress(address);
+        System.out.println("City: ");
+        String city = scan.next();
+        contacts.setState(city);
+        System.out.println("State: ");
+        String state = scan.next();
+        contacts.setState(state);
+        System.out.println("Zip: ");
+        String zip = scan.next();
+        contacts.setState(zip);
+        System.out.println("phone: ");
+        String phone = scan.next();
+        contacts.setState(phone);
+        System.out.println("Email: ");
+        String email = scan.next();
+        contacts.setState(email);
+
+        System.out.println(contacts.toString());
+
+    }
+
+    public static class AddressBook {
+        public void addContact() {
+        }
+    }
+}
